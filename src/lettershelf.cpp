660733647417e53f5bf222b8e2a7e41eb98b1537
Patrick Stapfer <p.stapfer@gmail.com>
2012-06-27 18:14:05 +0200

    Added LetterShelf class for input visualization

---
+++ src/lettershelf.cpp
@@ -0,0 +1,54 @@
+/**
+ *  WordGL
+ *  Copyright (C) 2012  Patrick Stapfer <p.stapfer@technikum-wien.at>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "lettershelf.h"
+
+#ifdef __APPLE__
+    #include <OpenGL/gl.h>
+#else
+    #include <GL/gl.h>
+#endif
+
+namespace WordGL {
+
+	LetterShelf::LetterShelf(){
+		
+	}
+	
+    LetterShelf::LetterShelf(Point startPoint, unsigned int max_input) {
+
+    }
+
+	LetterShelf::~LetterShelf() {
+
+    }
+	
+	void LetterShelf::push(char c){
+		
+	}
+	
+	void LetterShelf::pop(){
+		
+	}	
+	
+	void LetterShelf::clear(){
+		
+	}
+
+}
---
+++ src/lettershelf.h
@@ -0,0 +1,43 @@
+/**
+ *  WordGL
+ *  Copyright (C) 2012  Patrick Stapfer <p.stapfer@technikum-wien.at>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "point.h"
+#include "dimension.h"
+#include "cube.h"
+#include <vector>
+
+namespace WordGL {
+        
+    class LetterShelf {
+
+    public:
+		LetterShelf();
+        LetterShelf(Point startPoint, unsigned int max_input);
+        virtual ~LetterShelf();
+		void draw();
+
+    private:
+		std::vector<Cube> cubes;
+		void push(char c);
+		void pop();
+		void clear();
+    };
+
+}
